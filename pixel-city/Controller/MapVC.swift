c9aef9d3275cba615aa0dd7f4f7351ae851d7b42
Jesse Elkharrat <jessenelk@gmail.com>
2018-03-15 08:37:25 -0400

    S7L100

--- pixel-city/Controller/MapVC.swift
+++ pixel-city/Controller/MapVC.swift
@@ -15,23 +15,67 @@
 import UIKit
 import Alamofire
 import MapKit
+import CoreLocation
+
 
 
 class MapVC: UIViewController {
     
+    //Outlets
     @IBOutlet weak var mapView: MKMapView!
+    
+    //Variables
+    var locationManager = CLLocationManager()
+    
+    //Constants
+    let authorizationStatus = CLLocationManager.authorizationStatus()
+    let regionRadius: Double = 1000 //meters
     
     override func viewDidLoad() {
         super.viewDidLoad()
         mapView.delegate = self
+        locationManager.delegate = self
+        //need to go to info.plist + button and add "privacy - location always..." and "privacy -  when in use..." and "NSLocationAlwaysandWhenInUseUsaageDescription" and add descriptions of how app is using location 
+        configureLocationServices()
     }
 
     @IBAction func centerMapButtonWasPressed(_ sender: Any) {
+        if authorizationStatus == .authorizedAlways || authorizationStatus == .authorizedWhenInUse {
+            centerMapOnUserLocation()
+        }
     }
 }
 
 //Need to conform to MapViewDelegate
 extension MapVC: MKMapViewDelegate {
+    
+    //This function zooms the map into the location of the user for a better view
+    func centerMapOnUserLocation() {
+        guard let coordinate = locationManager.location?.coordinate else { return }
+        
+        //its one way so need to multiply to get both ways
+        let coordinateRegion = MKCoordinateRegionMakeWithDistance(coordinate, regionRadius * 2.0, regionRadius * 2.0)
+        mapView.setRegion(coordinateRegion, animated: true)
+    }
+    
+    
+}
+
+extension MapVC: CLLocationManagerDelegate {
+    //Checks to see if app is authorized to use location services
+    func configureLocationServices() {
+        if authorizationStatus == .notDetermined {
+            //going to request authorization always whether the app is open or not
+            locationManager.requestAlwaysAuthorization()
+        } else {
+            return
+        }
+    }
+    
+    //this function gets called any time map view authorization status is changed, so if wheninuselocationservices is approved, it will center on the user
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        centerMapOnUserLocation()
+    }
     
 }
 
